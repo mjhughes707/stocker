2caf85cd53df340e814d46c9d6755657a5f36a7a
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-06 14:38:01 -0800

    began Header component and brought in FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import Header from "./components/Header";
+import "./styles.css";
 
 function App() {
-  return <div>TEST</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
