c837aa0415afa30883595555f40497f4ed14130b
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-07 15:19:49 -0800

    added ability to retrieve tweets based on inputted stock symbol

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,23 +1,29 @@
 import React, { Fragment, useState } from "react";
 
 const Request = () => {
-  const [stockData, setStockData] = useState({
-    symbols: [],
-  });
+  const [symbols, setSymbols] = useState("");
 
-  const { symbols } = stockData;
+  const handleChange = e => setSymbols(e.target.value);
 
-  const handleChange = e =>
-    setStockData({
-      [e.target.name]: e.target.value,
-    });
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.stocktwits.com/api/2/streams/symbol/${symbols}.json`
+      );
+      const responseJSON = await response.json();
+      console.log(responseJSON);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Fragment>
       <h4 className="request-header">
         Please enter the stock symbol(s) for the tweets you wish to display.
       </h4>
-      <form className="request-form">
+      <form className="request-form" onSubmit={e => handleSubmit(e)}>
         <input
           className="request-field"
           type="text"
@@ -29,6 +35,7 @@ const Request = () => {
         <small>
           If multiple, please separate by a comma (e.g. AAPL, MSFT, TSLA)
         </small>
+        <input type="submit" className="btn" />
       </form>
     </Fragment>
   );
