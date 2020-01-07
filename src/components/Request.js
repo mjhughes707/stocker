f85acaa49aec202dbba5a65eb060054d535347e6
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-06 16:01:14 -0800

    built out Request component as controlled form

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,13 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Request = () => {
+  const [stockData, setStockData] = useState({
+    symbols: [],
+  });
+
+  const { symbols } = stockData;
+
+  const handleChange = e =>
+    setStockData({
+      [e.target.name]: e.target.value,
+    });
+
   return (
     <Fragment>
       <h4 className="request-header">
         Please enter the stock symbol(s) for the tweets you wish to display.
       </h4>
-      <form className="request-form" action="">
-        <input className="request-field" type="text" />
+      <form className="request-form">
+        <input
+          className="request-field"
+          type="text"
+          name="symbols"
+          value={symbols}
+          onChange={e => handleChange(e)}
+          required
+        />
         <small>
           If multiple, please separate by a comma (e.g. AAPL, MSFT, TSLA)
         </small>
