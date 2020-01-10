45de3e1da1245986d26e069e788119f149ad583d
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-09 16:21:02 -0800

    added ability to add tweets and display multiple listings

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ import "./styles.css";
 
 function App() {
   const [tweets, setTweets] = useState([]);
-  const [title, setTitle] = useState("");
 
   const getTweets = async symbols => {
     try {
       const res = await axios.get(
         `https://api.stocktwits.com/api/2/streams/symbol/${symbols}.json?limit=3`
       );
-      setTitle(res.data.symbol.title);
-      setTweets(res.data.messages);
+      addTweets(res.data.symbol.title, res.data.messages);
     } catch (err) {
       alert(
         `"${symbols.toUpperCase()}" not found. Please enter a valid stock symbol.`
@@ -24,22 +22,31 @@ function App() {
     }
   };
 
+  const addTweets = (title, messages) => {
+    const newTweets = [{ title: title, tweets: messages }, ...tweets];
+    setTweets(newTweets);
+  };
+
   return (
     <div>
       <Header />
       <main className="container">
         <Form getTweets={getTweets} />
         <section>
-          <h2 className="section-title">{title}</h2>
-          <p className="section-intro">
-            {tweets.length > 0
-              ? `The ${tweets.length} most recent tweets`
-              : "No tweets found"}
-          </p>
+          {tweets.map((obj, index) => (
+            <div key={index}>
+              <h2 className="section-title">{obj.title}</h2>
+              <p className="section-intro">
+                {tweets.length > 0
+                  ? `The ${obj.tweets.length} most recent tweets`
+                  : "No tweets found"}
+              </p>
+              {obj.tweets.map(tweet => (
+                <Display key={tweet.id} tweet={tweet} />
+              ))}
+            </div>
+          ))}
         </section>
-        {tweets.map(tweet => (
-          <Display key={tweet.id} tweet={tweet} />
-        ))}
       </main>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,15 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
+import PropTypes from "prop-types";
 
 const Form = ({ getTweets }) => {
   const [symbols, setSymbols] = useState("");
+  const textBoxRef = useRef(null);
 
   const handleSubmit = e => {
     e.preventDefault();
     getTweets(symbols);
+    setSymbols("");
+    textBoxRef.current.focus();
   };
 
   return (
@@ -15,6 +19,7 @@ const Form = ({ getTweets }) => {
       </h4>
       <form className="form-form" onSubmit={e => handleSubmit(e)}>
         <input
+          ref={textBoxRef}
           className="form-field"
           type="text"
           name="symbols"
@@ -27,6 +32,10 @@ const Form = ({ getTweets }) => {
       </form>
     </Fragment>
   );
+};
+
+Form.propTypes = {
+  getTweets: PropTypes.func.isRequired,
 };
 
 export default Form;
