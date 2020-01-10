1e7ba495f834b515ea88a9f90be92de3c0e40f1d
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-09 22:34:23 -0800

    Updates

---
+++ precache-manifest.fcbdb1af0304a9382d78a36b4206f1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395e8a891b60f39a6bdaaee93abaecff",
+    "url": "/stocker/index.html"
+  },
+  {
+    "revision": "546e1e6a1f140c9acb1b",
+    "url": "/stocker/static/css/main.f4748059.chunk.css"
+  },
+  {
+    "revision": "40cd8f36dad384a62bbe",
+    "url": "/stocker/static/js/2.0f6adcb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stocker/static/js/2.0f6adcb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "546e1e6a1f140c9acb1b",
+    "url": "/stocker/static/js/main.715b40fe.chunk.js"
+  },
+  {
+    "revision": "b2e209cedac480e1e20c",
+    "url": "/stocker/static/js/runtime-main.8c279654.js"
+  }
+]);
