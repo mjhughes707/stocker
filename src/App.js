e5b60c31e8c9e5c9e3a02dd666752bc491abda07
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-09 18:51:35 -0800

    added ability to remove tweets + corresponding styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Display from "./components/Display";
@@ -27,28 +27,38 @@ function App() {
     setTweets(newTweets);
   };
 
+  const removeTweets = index => {
+    const newTweets = [...tweets];
+    newTweets.splice(index, 1);
+    setTweets(newTweets);
+  };
+
   return (
-    <div>
+    <Fragment>
       <Header />
       <main className="container">
         <Form getTweets={getTweets} />
-        <section>
-          {tweets.map((obj, index) => (
-            <div key={index}>
-              <h2 className="section-title">{obj.title}</h2>
-              <p className="section-intro">
-                {tweets.length > 0
-                  ? `The ${obj.tweets.length} most recent tweets`
-                  : "No tweets found"}
-              </p>
-              {obj.tweets.map(tweet => (
-                <Display key={tweet.id} tweet={tweet} />
-              ))}
-            </div>
-          ))}
-        </section>
+        {tweets.map((obj, index) => (
+          <section key={index}>
+            <h2 className="section-title">{obj.title}</h2>
+            <p className="section-intro">
+              {tweets.length > 0
+                ? `The ${obj.tweets.length} most recent tweets`
+                : "No tweets found"}
+            </p>
+            <input
+              type="submit"
+              value="REMOVE"
+              className="remove-btn"
+              onClick={() => removeTweets(index)}
+            />
+            {obj.tweets.map(tweet => (
+              <Display key={tweet.id} tweet={tweet} />
+            ))}
+          </section>
+        ))}
       </main>
-    </div>
+    </Fragment>
   );
 }
 
